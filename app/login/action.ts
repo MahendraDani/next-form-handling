00a37b3b8a8ca12b35dffa006e6c990619f95051
Mahendra Dani <danimahendra0904@gmail.com>
2024-06-11 11:00:52 +0530

    build(fix): removed try-catch in login action

--- app/login/action.ts
+++ app/login/action.ts
@@ -5,6 +5,7 @@ import { validatePassword } from "@/lib/auth/password";
 import { database } from "@/lib/services";
 import { ZCreateAccountSchema } from "@/lib/zod";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export type TActionErrorType =
   | "validationError:email"
@@ -23,71 +24,62 @@ export const loginAction = async (
   prevState: IPrevStateLoginAction,
   formData: FormData
 ): Promise<IPrevStateLoginAction> => {
-  try {
-    const emailRaw = formData.get("email");
-    const passwordRaw = formData.get("password");
+  const emailRaw = formData.get("email");
+  const passwordRaw = formData.get("password");
 
-    const parsed = ZCreateAccountSchema.safeParse({
-      email: emailRaw,
-      password: passwordRaw,
-    });
+  const parsed = ZCreateAccountSchema.safeParse({
+    email: emailRaw,
+    password: passwordRaw,
+  });
 
-    if (!parsed.success) {
-      return {
-        message: parsed.error.errors[0].message,
-        isError: true,
-        errorType:
-          parsed.error.errors[0].path[0] === "email"
-            ? "validationError:email"
-            : "validationError:password",
-      };
-    }
-
-    const { account } = await database.accounts.getByEmail({
-      email: parsed.data.email,
-    });
-
-    if (!account) {
-      return {
-        message: "Account with provided not found, please signup!",
-        isError: true,
-        errorType: "databaseError",
-      };
-    }
-
-    if (
-      !validatePassword({
-        rawPassword: parsed.data.password,
-        hashedPassword: account.password,
-      })
-    ) {
-      return {
-        message: "Incorrect password",
-        isError: true,
-        errorType: "databaseError",
-      };
-    }
-    // if account exists set cookie login : true
-    const token = generateJWT({
-      payload: account,
-    });
-    // 15 days -> 15 * 24 * 60 * 60 * 1000
-    cookies().set("token", token, {
-      maxAge: 15 * 24 * 60 * 60 * 1000,
-      httpOnly: true,
-    });
+  if (!parsed.success) {
     return {
-      message: "Login successful",
-      isError: false,
-      errorType: null,
-    };
-  } catch (error) {
-    return {
-      message: "something went wrong",
+      message: parsed.error.errors[0].message,
       isError: true,
-      errorType: "internalError",
+      errorType:
+        parsed.error.errors[0].path[0] === "email"
+          ? "validationError:email"
+          : "validationError:password",
     };
   }
+
+  const { account } = await database.accounts.getByEmail({
+    email: parsed.data.email,
+  });
+
+  if (!account) {
+    return {
+      message: "Account with provided not found, please signup!",
+      isError: true,
+      errorType: "databaseError",
+    };
+  }
+
+  if (
+    !validatePassword({
+      rawPassword: parsed.data.password,
+      hashedPassword: account.password,
+    })
+  ) {
+    return {
+      message: "Incorrect password",
+      isError: true,
+      errorType: "databaseError",
+    };
+  }
+  const token = generateJWT({
+    payload: account,
+  });
+  // 15 days -> 15 * 24 * 60 * 60 * 1000
+  cookies().set("token", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+  });
+  redirect("/feedback");
+  return {
+    message: "Login successful",
+    isError: false,
+    errorType: null,
+  };
 };
 
 /*
